97bc818533138af8d50745b0adb6a87039de9f4b
maksim15777 <k25782176@gmail.com>
2026-07-18 09:30:03 +0300

    Fix CI: change xcodebuild scheme to 'BetterTG Release' and fix gyb call signature

--- gyb.sh
+++ gyb.sh
@@ -1,9 +1,9 @@
-mkdir BetterTG/Utilities/Generated
+mkdir -p BetterTG/Utilities/Generated
 cd BetterTG/Utilities/Templates
 find . -name "*.gyb" |
 while read file; do
     filename=$(echo "$file" | sed 's/.\///')
-    API_ID=$1 API_HASH=$2 gyb --line-directive '' -o "../Generated/${filename%.gyb}" "$filename";
+    API_ID=$1 API_HASH=$2 gyb -o "../Generated/${filename%.gyb}" "$filename";
 done
 
 cd ../..
